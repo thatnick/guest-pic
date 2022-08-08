d9947d666b5aef902418c8c820f66c8d6a7f2f42
Nicholas Russell <thatnick@me.com>
2022-08-08 14:42:43 +0100

    Create project structure

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import tw from "twrnc";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={tw`pt-6 bg-green-100`}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
