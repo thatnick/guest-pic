90ff1d4069265afda59e8d23834b17afa1d3c906
AdilBulsari <adilbulsari@gmail.com>
2022-08-09 16:30:38 +0100

    added upload picture

--- firebase/storage.ts
+++ firebase/storage.ts
@@ -1,28 +1,12 @@
-import { getStorage, ref, uploadString } from "firebase/storage";
-import * as FileSystem from "expo-file-system";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 import { app } from "./firebaseApp";
 const storage = getStorage(app);
 
 export const uploadPhoto = async (uri: string) => {
-  var filename = "test.jpg";
-
-  const storageRef = ref(storage, `images/${filename}`);
-
-  console.log(uri, "<< uri in upload func");
-  // let photoString = await FileSystem.readAsStringAsync(uri, {
-  //   encoding: FileSystem.EncodingType.Base64,
-  // });
-  //console.log(photoString, "this should be a giant string");
-  const metadata = {
-    contentType: "image/jpeg",
-  };
-  await uploadString(
-    storageRef,
-    "data:image/jpg;base64," + uri,
-    "base64",
-    metadata
-  ).then((snapshot) => {
-    console.log("Uploaded a base64 string!");
-  });
+  const storage = getStorage();
+  const refs = ref(storage, "image.jpg");
+  const img = await fetch(uri);
+  const bytes = await img.blob();
+  await uploadBytes(refs, bytes);
 };
