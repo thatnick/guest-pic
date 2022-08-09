b5945cf91b6485a99a72276e59ef4203ae806016
Euan Macfarlane <euanmacfarlane@macbook-pro.lan>
2022-08-09 10:53:41 +0100

    component accesses camera from device

--- App.tsx
+++ App.tsx
@@ -1,15 +1,114 @@
 import tw from "twrnc";
 import { StatusBar } from "expo-status-bar";
-import { View, Text, SafeAreaView } from "react-native";
+import { StyleSheet, View, Text, SafeAreaView, TouchableOpacity, Alert } from "react-native";
+import React from 'react';
+import {useState} from 'react'
+import {Camera, CameraType} from 'expo-camera'
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <View style={tw`pt-6 bg-green-100`}>
-        <Text>Open up App.tsx to start working on your app!</Text>
 
-        <StatusBar style="auto" />
-      </View>
+  const [startCamera,setStartCamera] = useState(false)
+  let camera: Camera
+  const __startCamera = async () => {
+    const {status} = await Camera.requestCameraPermissionsAsync()
+    if (status === 'granted') {
+      // start the camera
+      setStartCamera(true)
+    } else {
+      Alert.alert('Access denied')
+    }
+  }
+
+  const __takePicture = async () => {
+    if (!camera) return
+    const photo = await camera.takePictureAsync()
+   
+  }
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {startCamera ? (
+        <Camera
+          style={{flex: 1,width:"100%"}}
+          // type={CameraType.back}
+          ref={(r) => {
+            camera = r
+          }}
+        ></Camera>
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: '#fff',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <TouchableOpacity
+            onPress={__startCamera}
+            style={{
+              width: 130,
+              borderRadius: 4,
+              backgroundColor: '#14274e',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 40
+            }}
+          >
+            <Text
+              style={{
+                color: '#fff',
+                fontWeight: 'bold',
+                textAlign: 'center'
+              }}
+            >
+              Take picture
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
+<View
+        style={{
+        position: 'absolute',
+        bottom: 0,
+        flexDirection: 'row',
+        flex: 1,
+        width: '100%',
+        padding: 20,
+        justifyContent: 'space-between'
+        }}
+      >
+        <View
+        style={{
+        alignSelf: 'center',
+        flex: 1,
+        alignItems: 'center'
+        }}
+        >
+            <TouchableOpacity
+            onPress={__takePicture}
+            style={{
+            width: 70,
+            height: 70,
+            bottom: 0,
+            borderRadius: 50,
+            backgroundColor: '#fff'
+            }}
+            />
+    </View>
+    </View>
+      <StatusBar style="auto" />
     </SafeAreaView>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
