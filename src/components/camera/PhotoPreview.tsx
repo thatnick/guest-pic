fe0afacd8d294c75bb9b34d48b86c0fa3d531208
Nicholas Russell <thatnick@me.com>
2022-08-13 21:30:56 +0100

    Fix positioning of save/retake camera buttons

--- src/components/camera/PhotoPreview.tsx
+++ src/components/camera/PhotoPreview.tsx
@@ -1,6 +1,11 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { useContext } from "react";
-import { TouchableOpacity, ImageBackground, StyleSheet } from "react-native";
+import {
+  TouchableOpacity,
+  ImageBackground,
+  StyleSheet,
+  View,
+} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { EventContext } from "../../contexts";
 import { addPhotoToEvent } from "../../firebase/db";
@@ -13,31 +18,36 @@ export default function PhotoPreview({ route }) {
 
   return (
     <ImageBackground
-      style={{ height: "100%", width: "100%" }}
+      style={{
+        height: "100%",
+        width: "100%",
+      }}
       source={{ uri: photo.path }}
     >
-      <TouchableOpacity style={styles.retake}>
-        <Icon
-          name={"remove"}
-          size={50}
-          color="red"
-          onPress={() => navigation.goBack()}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.save}>
-        <Icon
-          name={"check"}
-          size={50}
-          color="green"
-          onPress={async () => {
-            console.log("PATH" + photo.path);
+      <View style={styles.content}>
+        <TouchableOpacity style={styles.retake}>
+          <Icon
+            name={"remove"}
+            size={50}
+            color="red"
+            onPress={() => navigation.goBack()}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.save}>
+          <Icon
+            name={"check"}
+            size={50}
+            color="green"
+            onPress={async () => {
+              console.log("PATH" + photo.path);
 
-            await uploadPhoto(photo.path);
-            addPhotoToEvent({ eventId: event, photoPath: photo.path });
-            navigation.goBack();
-          }}
-        />
-      </TouchableOpacity>
+              await uploadPhoto(photo.path);
+              addPhotoToEvent({ eventId: event, photoPath: photo.path });
+              navigation.goBack();
+            }}
+          />
+        </TouchableOpacity>
+      </View>
     </ImageBackground>
   );
 }
@@ -45,21 +55,16 @@ export default function PhotoPreview({ route }) {
 const styles = StyleSheet.create({
   content: {
     flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "flex-end",
+    flexDirection: "row",
+    alignItems: "flex-end",
     padding: 50,
   },
   retake: {
     display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignSelf: "flex-start",
+    width: "50%",
   },
   save: {
     display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignSelf: "flex-end",
+    width: "50%",
   },
 });
