ab2015fc710176650a8dcf30999d045f91fcc7be
Nicholas Russell <thatnick@me.com>
2022-08-13 21:05:51 +0100

    Add context types

--- src/utilities/types.ts
+++ src/utilities/types.ts
@@ -1,3 +1,5 @@
+import { Dispatch, SetStateAction } from "react";
+
 export interface User {
   email: string;
   name: string;
@@ -19,9 +21,19 @@ export interface ItineraryItem {
   title: string;
   description: string;
   location: string;
-  time: string;
+  time: Date;
 }
 
 export interface Props<T> {
   object: T;
+}
+
+export interface EventContextType {
+  event: Event;
+  setEvent: (prevEvent: Event) => void;
+}
+
+export interface UserContextType {
+  user: User;
+  setUser: (prevUser: User) => void;
 }
