79994c159df18911dd9d78d145eb67af0e215956
PDE24 <95310165+PDE24@users.noreply.github.com>
2022-08-15 14:12:38 +0100

    datepicker

--- src/components/events/CreateEventForm.tsx
+++ src/components/events/CreateEventForm.tsx
@@ -1,10 +1,21 @@
 import { useNavigation } from "@react-navigation/native";
-import DatePicker from "react-native-datepicker";
+//import DatePicker from "react-native-datepicker";
+import DatePicker from "react-native-date-picker";
 import React, { useState, useContext } from "react";
-import { Text, View, TextInput, Button } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
 import { UserContext } from "../../contexts";
-import { addEvent, addGuestToEvent, addItineraryItemToEvent } from "../../firebase/db";
-import moment from 'moment';
+import {
+  addEvent,
+  addGuestToEvent,
+  addItineraryItemToEvent,
+} from "../../firebase/db";
 
 export default function CreateEventForm() {
   const navigation = useNavigation();
@@ -15,13 +26,12 @@ export default function CreateEventForm() {
   const [eventLocation, setEventLocation] = useState("");
   const [eventDescription, setEventDescription] = useState("");
   const [banner, setBanner] = useState("");
-  const [eventDate, setEventDate] = useState("");
-  
+  const [eventDate, setEventDate] = useState(new Date());
+
   // Itinerary
   const [itineraryTitle, setItineraryTitle] = useState("");
   const [itineraryLocation, setItineraryLocation] = useState("");
   const [itineraryDescription, setItineraryDescription] = useState("");
-  const [itineraryTime, setItineraryTime] = useState("");
 
   const handleRegisterPress = async () => {
     const eventToAdd = {
@@ -31,7 +41,6 @@ export default function CreateEventForm() {
       date: eventDate,
       bannerUrl: banner,
     };
-    
 
     const newEvent = await addEvent(eventToAdd);
     if (!newEvent) throw new Error("Cannot add guest to event");
@@ -45,7 +54,7 @@ export default function CreateEventForm() {
       eventId: newEvent.id,
       title: itineraryTitle,
       location: itineraryLocation,
-      time: itineraryTime,
+      time: eventDate,
       description: itineraryDescription,
     });
 
@@ -53,86 +62,65 @@ export default function CreateEventForm() {
   };
 
   return (
-    <View>
-      <Button title="Cancel" onPress={() => navigation.goBack()} ></Button>
-      <View>
-      <Text>Event</Text>
-      <Text>Title:</Text>
-      <TextInput onChangeText={(newText) => setEventTitle(newText)}></TextInput>
+    <SafeAreaView>
+      <ScrollView>
+        <Button title="Cancel" onPress={() => navigation.goBack()}></Button>
+        <View>
+          <Text>Event</Text>
+          <Text>Title:</Text>
+          <TextInput
+            onChangeText={(newText) => setEventTitle(newText)}
+          ></TextInput>
 
-      <Text>Location:</Text>
-      <TextInput onChangeText={(newText) => setEventLocation(newText)}></TextInput>
+          <Text>Location:</Text>
+          <TextInput
+            onChangeText={(newText) => setEventLocation(newText)}
+          ></TextInput>
 
-      <Text>Description:</Text>
-      <TextInput
-        onChangeText={(newText) => setEventDescription(newText)}
-      ></TextInput>
+          <Text>Description:</Text>
+          <TextInput
+            onChangeText={(newText) => setEventDescription(newText)}
+          ></TextInput>
 
-      <Text>Banner url:</Text>
-      <TextInput onChangeText={(newText) => setBanner(newText)}></TextInput>
+          <Text>Banner url:</Text>
+          <TextInput onChangeText={(newText) => setBanner(newText)}></TextInput>
 
-      <Text>Event Date:</Text>
-      <DatePicker
-        style={{ width: 200 }}
-        date={eventDate}
-        mode="date"
-        format="DD-MM-YYYY"
-        confirmBtnText="Confirm"
-        cancelBtnText="Cancel"
-        customStyles={{
-          dateIcon: {
-            position: "absolute",
-            left: 0,
-            top: 4,
-            marginLeft: 0,
-          },
-          dateInput: {
-            marginLeft: 30,
-          },
-        }}
-        onDateChange={(confirmTime) => {
-          setEventDate(confirmTime);
-        }}
-      />
-      </View>
+          <Text>Event Date:</Text>
+          <DatePicker
+            date={eventDate}
+            onDateChange={setEventDate}
+            mode={"date"}
+            minimumDate={new Date()}
+          />
+        </View>
 
-      <View>
-      <Text>Add your first Itinerary Item</Text>
-      <Text>Title:</Text>
-      <TextInput onChangeText={(newText) => setItineraryTitle(newText)}></TextInput>
+        <View>
+          <Text>Add your first Itinerary Item</Text>
+          <Text>Title:</Text>
+          <TextInput
+            onChangeText={(newText) => setItineraryTitle(newText)}
+          ></TextInput>
 
-      <Text>Location:</Text>
-      <TextInput onChangeText={(newText) => setItineraryLocation(newText)}></TextInput>
+          <Text>Location:</Text>
+          <TextInput
+            onChangeText={(newText) => setItineraryLocation(newText)}
+          ></TextInput>
 
-      <Text>Descrition:</Text>
-      <TextInput onChangeText={(newText) => setItineraryDescription(newText)}></TextInput>
+          <Text>Descrition:</Text>
+          <TextInput
+            onChangeText={(newText) => setItineraryDescription(newText)}
+          ></TextInput>
 
-      <Text>Start Time:</Text>
-      <DatePicker
-        style={{ width: 200 }}
-        date={itineraryTime}
-        mode="time"
-        format="LT"
-        confirmBtnText="Confirm"
-        cancelBtnText="Cancel"
-        customStyles={{
-          dateIcon: {
-            position: "absolute",
-            left: 0,
-            top: 4,
-            marginLeft: 0,
-          },
-          dateInput: {
-            marginLeft: 30,
-          },
-        }}
-        onDateChange={(confirmTime) => {
-          setItineraryTime(confirmTime);
-        }}
-      />
-      </View>
+          <Text>Start Time:</Text>
+          <DatePicker
+            date={eventDate}
+            onDateChange={setEventDate}
+            mode={"time"}
+          />
+        </View>
 
-      <Button title="Create event" onPress={handleRegisterPress}></Button>
-    </View>
+        <Button title="Create event" onPress={handleRegisterPress}></Button>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
