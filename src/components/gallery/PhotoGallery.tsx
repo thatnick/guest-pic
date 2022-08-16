7cfc3f67af95b5de658a88cf4ab5856f61f0e1c3
PDE24 <95310165+PDE24@users.noreply.github.com>
2022-08-16 15:43:58 +0100

    add multi line photo preview

--- src/components/gallery/PhotoGallery.tsx
+++ src/components/gallery/PhotoGallery.tsx
@@ -7,25 +7,56 @@ interface Props {
   photosCallback: () => Promise<Photo[]>;
 }
 const PhotoGallery = ({ photosCallback }: Props) => {
-  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [photos, setPhotos] = useState<Photo[][]>([]);
 
   useEffect(() => {
-    photosCallback().then((data) => setPhotos(data));
+    photosCallback().then((data) => {
+      const pairs = [];
+
+      if (data.length % 2 === 0) {
+        for (let i = 0; i < data.length; i + 2) {
+          const pair = [data[i], data[i + 1]];
+          pairs.push(pair);
+        }
+      } else {
+        data.push({
+          downloadUrl:
+            "https://firebasestorage.googleapis.com/v0/b/guestpic.appspot.com/o/photos%2Fimages-4.jpeg?alt=media&token=5460b2a7-ee3b-4840-bdb9-2c5e47be37b0",
+          id: "Test",
+          userEmail: "test@s.com",
+        });
+        for (let i = 0; i < data.length; i += 2) {
+          const pair = [data[i], data[i + 1]];
+          pairs.push(pair);
+        }
+      }
+
+      setPhotos(pairs);
+    });
+  }, []);
+
+
+  const renderItem = useCallback(({ item }) => {
+    return (
+      <View>
+        <Image
+          source={{ uri: item[0].downloadUrl }}
+          style={{ height: 60, width: 60, margin: 2 }}
+        />
+        <Image
+          source={{ uri: item[1].downloadUrl }}
+          style={{ height: 60, width: 60, margin: 2 }}
+        />
+      </View>
+    );
   }, []);
 
   return (
-
     <View>
       <FlatList
-      horizontal={true}
+        horizontal={true}
         data={photos}
-        renderItem={({ item: photo }) => {
-              return <Image
-                source={{ uri: photo.downloadUrl }}
-                style={{ height: 100, width: 100, margin: 2 }}
-              />
-            }}
-        
+        renderItem={renderItem}
       />
     </View>
   );
